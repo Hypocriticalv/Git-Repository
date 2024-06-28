2f7650e1807178e4f87f7df3dbd15ebddc8b5dca
Vimal <vims@live.com.au>
2024-06-28 10:26:47 +1000

    Modified for user input in port scanning range

--- Network Port Scanner.py
+++ Network Port Scanner.py
@@ -7,15 +7,27 @@ from datetime import datetime
 ascii_banner = pyfiglet.figlet_format("Port Scanner")
 print(ascii_banner)
 #defining a target for running script, one argument is the script name and the other is hostname/target IP
-if len(sys.argv) == 2:
+if len(sys.argv) == 4:
     try:
         target = socket.gethostbyname(sys.argv[1])
+        start_port = int(sys.argv[2])
+        end_port = int(sys.argv[3])
+        if start_port < 1 or end_port > 65535:
+            print('Invalid range, please select a port between 1 and 65535.')
+            sys.exit()
+        if start_port > end_port:
+            print('End port cannot be greater than start port.')
+            sys.exit()
+        elif start_port and end_port == int():
+            print(f'Thank you for selecting port range between {start_port} and {end_port}.')       
     except socket.gaierror:
         print('Hostname could not be resolved, exiting program.')
         sys.exit()
+    except ValueError:
+        print('Please select a port between 1 and 65535.')
 else:
     print('Invalid amount of arguments.')
-    print('You can use this script with the following command: python "Network Port Scanner.py" <hostname>')
+    print('You can use this script with the following command: python "Network Port Scanner.py" <hostname> <port 1> <port 2>')
     sys.exit()
     
 #adding a banner notifying the user about target being scanned + time the scan was started
@@ -28,7 +40,7 @@ counter = 0
 #adding in port range to be scanned and executing scan, ports can range from 1 - 65535
 try:
     
-    for port in range(1,140):
+    for port in range(start_port, end_port + 1):
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = s.connect_ex((target,port))
