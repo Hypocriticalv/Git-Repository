a0eb92f588281c5ec70063d444f324b846a1ddef
Vimal <vims@live.com.au>
2024-07-02 14:00:06 +1000

    Modified to parse URL to find hostname and resolve for IP address.

--- Website IP Finder.py
+++ Website IP Finder.py
@@ -1,14 +1,26 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#module import for parsing url links
+from urllib.parse import urlparse 
+
+#list of accepted inputs
+list = ['y','Y','Yes','YES','yes'] 
+
 #AF_INET refers to address-family ipv4, sock_stream refers to TCP protocol
-list = ['y','Y','Yes','YES','yes']
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
 while True:
-    ip = input('Site name: ')
+    url = input('Site name: ')
     try:
-        result = socket.gethostbyname(ip)
-        print(result)
+        #urlparse gets hostname from ip input
+        parsed_url = urlparse(url)
+        hostname = parsed_url.hostname or url
+        #Result gets the argument returned by parsed_url or ip
+        result = socket.gethostbyname(hostname)
+        print(f'The IP located at {hostname} is {result}.')
     except socket.gaierror:
             print('Invalid site name, try again.')
+            
     reuse = input('Run again? :')
     if reuse not in list:
         break
